a40802e85cd1c4e7187560dbc0839bbe3f5f2a63
masterghost2002 <rakeshdhariwal61@gmail.com>
2024-01-31 20:43:32 +0530

    base setup

---
+++ app/site/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="">
+      <section
+        className="h-full w-full pt-36 realtive flex flex-col items-center justify-center"
+      >
+
+      </section>
+    </main>
+  );
+}
